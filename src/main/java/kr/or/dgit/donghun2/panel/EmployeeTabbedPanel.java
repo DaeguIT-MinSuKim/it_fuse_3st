63b235f42159d07ad17ece35bd915447827d67b0
seol24 <ehdgnsdms321@gmail.com>
2017-04-07 16:02:17 +0900

    사원 이름 제약 조건 넣었습니다.

--- src/main/java/kr/or/dgit/donghun2/panel/EmployeeTabbedPanel.java
+++ src/main/java/kr/or/dgit/donghun2/panel/EmployeeTabbedPanel.java
@@ -3,6 +3,7 @@ package kr.or.dgit.donghun2.panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
@@ -12,9 +13,8 @@ import kr.or.dgit.donghun2.dto.Employee;
 import kr.or.dgit.donghun2.service.EmployeeService;
 import kr.or.dgit.donghun2.table.EmployeeTable;
 
-public class EmployeeTabbedPanel extends JPanel implements ActionListener{
+public class EmployeeTabbedPanel extends JPanel implements ActionListener {
 
-	
 	private EmployeePanel pEmpp;
 	private EmployeeService edao;
 	private JPanel pbtn;
@@ -28,7 +28,7 @@ public class EmployeeTabbedPanel extends JPanel implements ActionListener{
 		add(pEmpp);
 		pbtn = new JPanel();
 		add(pbtn);
-		
+
 		EbtnSave = new JButton("저장");
 		EbtnDele = new JButton("삭제");
 		EbtnSearch = new JButton("검색");
@@ -38,31 +38,32 @@ public class EmployeeTabbedPanel extends JPanel implements ActionListener{
 		EbtnSearch.addActionListener(this);
 		pbtn.add(EbtnDele);
 		pbtn.add(EbtnSearch);
-		
+
 		pEmpt = new EmployeeTable();
 		add(pEmpt);
-		
+
 		setInit();
-		
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		 if (e.getSource() == EbtnSave) {
-	         actionPerformedEBtnSave(e);
-	      }
-	      if (e.getSource() == EbtnDele) {
-	         actionPerformedEBtnDele(e);
-	      }
-	      if (e.getSource() == EbtnSearch) {
-	         actionPerformedEbtnSearch(e);
-	      }
+		if (e.getSource() == EbtnSave) {
+			actionPerformedEBtnSave(e);
+		}
+		if (e.getSource() == EbtnDele) {
+			actionPerformedEBtnDele(e);
+		}
+		if (e.getSource() == EbtnSearch) {
+			actionPerformedEbtnSearch(e);
+		}
 	}
-	protected void setInit(){
+
+	protected void setInit() {
 		List<Employee> employee = edao.getInstance().selectEmployeeByAll();
-		
-		String lastNum = employee.get(employee.size()-1).getCode();
-		String value= String.format(setNoFormat(), Integer.parseInt(lastNum.substring(1))+1);
+
+		String lastNum = employee.get(employee.size() - 1).getCode();
+		String value = String.format(setNoFormat(), Integer.parseInt(lastNum.substring(1)) + 1);
 		pEmpp.getpCode().setTfValue(value);
 		pEmpp.getpCode().setFocusable(true);
 	}
@@ -71,46 +72,50 @@ public class EmployeeTabbedPanel extends JPanel implements ActionListener{
 		return "E%03d";
 	}
 
+	boolean EmployeeName = Pattern.matches("^[a-zA-Z가-힣]*$", pEmpp.getObject().getName());
+
 	private void actionPerformedEbtnSearch(ActionEvent e) {
-		 Employee res = edao.getInstance().selectEmployeeByNo(pEmpp.getObject());
-	      if (res == null) {
-	         JOptionPane.showMessageDialog(null, "검색결과가 없습니다.");
-	      } else {
-	         JOptionPane.showMessageDialog(null, "검색하였습니다.");
-	         pEmpp.setObject(res);
-	      }
+		Employee res = edao.getInstance().selectEmployeeByNo(pEmpp.getObject());
+		if (res == null) {
+			JOptionPane.showMessageDialog(null, "검색결과가 없습니다.");
+		} else {
+			JOptionPane.showMessageDialog(null, "검색하였습니다.");
+			pEmpp.setObject(res);
+		}
 	}
 
 	private void actionPerformedEBtnDele(ActionEvent e) {
 		int res = edao.getInstance().deleteEmployee(pEmpp.getObject());
-	      if (res == 0) {
-	         JOptionPane.showMessageDialog(null, "삭제안댐");
-	      } else {
-	         JOptionPane.showMessageDialog(null, "삭제댐");
-	         pEmpt.loadDate();
-	      }
+		if (res == 0) {
+			JOptionPane.showMessageDialog(null, "삭제안댐");
+		} else {
+			JOptionPane.showMessageDialog(null, "삭제댐");
+			pEmpt.loadDate();
+		}
 	}
 
 	private void actionPerformedEBtnSave(ActionEvent e) {
-		  if (pEmpp.isEmpty()) {
-		         JOptionPane.showMessageDialog(null, "빈칸이 있습니다.");
-		      } else {
-		         edao.getInstance().insertEmployee(pEmpp.getObject());
-		         JOptionPane.showMessageDialog(null, "저장되었습니다.");
-		         pEmpt.loadDate();
-		      }
+		if (pEmpp.isEmpty()) {
+			JOptionPane.showMessageDialog(null, "빈칸이 있습니다.");
+		} else if(EmployeeName == false) {
+			JOptionPane.showMessageDialog(null, "이름을 입력하세요");
+		} else  {
+			edao.getInstance().insertEmployee(pEmpp.getObject());
+			JOptionPane.showMessageDialog(null, "저장되었습니다.");
+			pEmpt.loadDate();
+		}
 	}
-	public JButton getEBtnSave(){
+
+	public JButton getEBtnSave() {
 		return EbtnSave;
 	}
-	public JButton getEBtnDele(){
+
+	public JButton getEBtnDele() {
 		return EbtnDele;
 	}
-	public JButton getEBtnSearch(){
+
+	public JButton getEBtnSearch() {
 		return EbtnSearch;
 	}
-	
-	
-	
 
 }
